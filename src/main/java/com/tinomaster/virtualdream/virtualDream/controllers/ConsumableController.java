2ebd24c02f23c9fae3c953c6df254f8f21d64e58
TinoMaster <ommallono@gmail.com>
2025-01-05 14:34:40 +0100

    added all features for consumable, all crud system

--- src/main/java/com/tinomaster/virtualdream/virtualDream/controllers/ConsumableController.java
+++ src/main/java/com/tinomaster/virtualdream/virtualDream/controllers/ConsumableController.java
@@ -1,8 +1,15 @@
 package com.tinomaster.virtualdream.virtualDream.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +30,28 @@ public class ConsumableController {
 	private final ConsumableService consumableService;
 	private final ModelMapper mapper;
 
+	private ConsumableDto consumableToConsumableDto(Consumable consumable) {
+		return mapper.map(consumable, ConsumableDto.class);
+	}
+
+	@GetMapping("/admin/consumable/list/{id}")
+	public ResponseEntity<ResponseBody<List<ConsumableDto>>> getAllByBusinessId(@PathVariable Long id) {
+		var consumableList = StreamSupport.stream(consumableService.getConsumablesByBusinessId(id).spliterator(), false)
+				.toList();
+		List<ConsumableDto> consumables = consumableList.stream().map(this::consumableToConsumableDto)
+				.collect(Collectors.toList());
+		return ResponseType.ok("successfullyRequest", consumables);
+	}
+
 	@PostMapping("/admin/consumable")
 	public ResponseEntity<ResponseBody<ConsumableDto>> saveConsumable(@RequestBody ConsumableDto consumableDto) {
 		Consumable consumableSaved = consumableService.saveConsumable(consumableDto);
 		return ResponseType.ok("successfullySaved", mapper.map(consumableSaved, ConsumableDto.class));
+	}
+
+	@DeleteMapping("/admin/consumable/{id}")
+	public ResponseEntity<ResponseBody<Object>> deleteConsumable(@PathVariable Long id) {
+		consumableService.deleteConsumable(id);
+		return ResponseType.ok("successfullyDeleted");
 	}
 }
--- src/main/java/com/tinomaster/virtualdream/virtualDream/services/ConsumableService.java
+++ src/main/java/com/tinomaster/virtualdream/virtualDream/services/ConsumableService.java
@@ -1,5 +1,7 @@
 package com.tinomaster.virtualdream.virtualDream.services;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +23,15 @@ public class ConsumableService {
 				.orElseThrow(() -> new RuntimeException("no se encuentra el consumable con id: " + id));
 	}
 
+	public List<Consumable> getConsumablesByBusinessId(Long businessId) {
+		return consumableRepository.findByBusinessId(businessId);
+	}
+
 	public Consumable saveConsumable(ConsumableDto consumableDto) {
 		return consumableRepository.save(mapper.map(consumableDto, Consumable.class));
+	}
+
+	public void deleteConsumable(Long id) {
+		consumableRepository.deleteById(id);
 	}
 }
